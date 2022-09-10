c9a8f71500500acb81bef039fbd1f33a3cb70014
Reinaldo44 <reinaldoap82@hotmail.com>
2022-09-10 20:11:40 -0300

    update

--- src/app/icons.ts
+++ src/app/icons.ts
@@ -6,51 +6,51 @@ export const icons : Iicons[] = [
    
     {
         id:1,
-        image: "/assets/icon_angular.png"
+        image: "./assets/icon_angular.png"
     },
     {
         id:2,
-        image: "/assets/icon_java.png"
+        image: "./assets/icon_java.png"
     },
     {
         id:3,
-        image: "/assets/icon-html.png"
+        image: "./assets/icon-html.png"
     },
     {
         id:4,
-        image: "/assets/icon-css.png"
+        image: "./assets/icon-css.png"
     },
     {
         id:5,
-        image: "/assets/icon-javascript.png"
+        image: "./assets/icon-javascript.png"
     },
     {
         id:6,
-        image: "/assets/icon-spring.png"
+        image: "./assets/icon-spring.png"
     },
     {
         id:7,
-        image: "/assets/icon-typescript.png"
+        image: "./assets/icon-typescript.png"
     },
     {
         id:8,
-        image: "/assets/icon-banco.png"
+        image: "./assets/icon-banco.png"
     },
     {
-        id:7,
-        image: "/assets/icon-typescript.png"
+        id:9,
+        image: "./assets/icon-typescript.png"
     },
     {
-        id:8,
-        image: "/assets/icon-mysql.png"
+        id:10,
+        image: "./assets/icon-mysql.png"
     },
     {
-        id:8,
-        image: "/assets/icon-sql.png"
+        id:11,
+        image: "./assets/icon-sql.png"
     },
     {
-        id:8,
-        image: "/assets/icon-ionic.png"
+        id:12,
+        image: "./assets/icon-ionic.png"
     }
 ]
 
